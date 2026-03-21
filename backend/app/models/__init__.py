9d478399dcc6f10616e47042ca782cf9e76d554d
torviktor <ckkbiouo@gmail.com>
2026-03-21 16:45:26 +0300

    feat: news auto-fetcher telegram and dss-pp

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,3 +3,4 @@ from app.models.certification import Certification, CertificationResult, Notific
 from app.models.achievement import AthleteAchievement
 from app.models.camp import Camp, CampParticipant
 from app.models.hall_of_fame import HallOfFame
+from app.models.competition_file import CompetitionFile
